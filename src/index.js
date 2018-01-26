84d8427a417636ca649cc0ef1ea99c2ff0b9211b
React Dev <mono_devok1111@protonmail.com>
2018-01-26 05:55:46 -0500

    update example

--- src/index.js
+++ src/index.js
@@ -241,6 +241,61 @@ class LoginControl extends React.Component {
     );
   }
 }
+
+
+function Mailbox(props) {
+  const unreadMessages = props.unreadMessages;
+  return (
+    <div>
+      <h1>Hello!</h1>
+      {unreadMessages.length > 0 &&
+        <h2>
+          You have {unreadMessages.length} unread messages.
+        </h2>
+      }
+    </div>
+  );
+}
+
+const messages = ['React', 'Re: React', 'Re:Re: React'];
+
+
+function WarningBanner(props) {
+  if (!props.warn) {
+    return null;
+  }
+
+  return (
+    <div className="warning">
+      Warning!
+    </div>
+  );
+}
+
+class Page extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {showWarning: true}
+    this.handleToggleClick = this.handleToggleClick.bind(this);
+  }
+
+  handleToggleClick() {
+    this.setState(prevState => ({
+      showWarning: !prevState.showWarning
+    }));
+  }
+
+  render() {
+    return (
+      <div>
+        <WarningBanner warn={this.state.showWarning} />
+        <button onClick={this.handleToggleClick}>
+          {this.state.showWarning ? 'Hide' : 'Show'}
+        </button>
+      </div>
+    );
+  }
+}
 // ========================================
 
 // ReactDOM.render(
@@ -254,9 +309,13 @@ const element = (
     <Clock />
     <Game />
     <LoginControl />
+    <Mailbox unreadMessages={messages} />
+    <Page />
   </div>
   
 );
+
+
 ReactDOM.render(
   element,
   document.getElementById('root')
