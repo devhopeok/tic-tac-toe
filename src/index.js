3bba61ad20be332b6cb43d66d590aaa724f5f143
React Dev <mono_devok1111@protonmail.com>
2018-01-26 05:27:55 -0500

    add conditional event

--- src/index.js
+++ src/index.js
@@ -175,6 +175,22 @@ class Clock extends React.Component {
   }
 }
 
+function UserGreeting(props) {
+  return <h1>Welcome back!</h1>;
+}
+
+function GuestGreeting(props) {
+  return <h1>Please sign up.</h1>;
+}
+
+function Greeting(props) {
+  const isLoggedIn = props.isLoggedIn;
+  if (isLoggedIn) {
+    return <UserGreeting />;
+  }
+  return <GuestGreeting />;
+}
+
 // ========================================
 
 // ReactDOM.render(
@@ -186,7 +202,10 @@ class Clock extends React.Component {
 const element = (
   <div>
     <Clock />
+    <Clock />
     <Game />
+    <Game />
+    <Greeting isLoggedIn={false} />
   </div>
   
 );
